10814e42e847c2fd1549e9a7db8dfa06660fa653
ashishkr710 <77523570+ashishkr710@users.noreply.github.com>
2023-03-16 20:35:21 +0530

    1

---
+++ src/main/java/com/Hostel/main/controller/LoginController.java
@@ -0,0 +1,28 @@
+package com.Hostel.main.controller;
+
+import com.Hostel.main.repository.RoleRepository;
+import com.Hostel.main.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    RoleRepository roleRepository;
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String registerGet(){
+        return "register";
+    }
+}
